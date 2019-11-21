3ff51770f2e6ea41191deede234057e8aa83ec5a
ivanfsilva <ivanfs27@gmail.com>
2019-11-21 19:15:18 -0300

    Refatorando CriadorDeSerie

--- app/Services/CriadorDeSerie.php
+++ app/Services/CriadorDeSerie.php
@@ -3,23 +3,45 @@
 namespace App\Services;
 
 use App\Serie;
+use Illuminate\Support\Facades\DB;
 use PhpParser\Node\Scalar\String_;
 
 class CriadorDeSerie
 {
     public function criarSerie(String $nomeSerie, int $qtdTemporadas, int $epPorTemporada): Serie
     {
+
+        DB::beginTransaction();
         $serie = Serie::create(['nome' => $nomeSerie]);
-
-        for ($i=1; $i <= $qtdTemporadas; $i++) {
-            $temporada = $serie->temporadas()->create(['numero' => $i]);
-
-            for ($j=1; $j <= $epPorTemporada; $j++) {
-                $temporada->episodios()->create(['numero' => $j]);
-            }
-
-        }
+        $this->criaTemporadas( $qtdTemporadas, $serie, $epPorTemporada );
+        DB::commit();
 
         return $serie;
+    }
+
+    /**
+     * @param int $qtdTemporadas
+     * @param $serie
+     * @param int $epPorTemporada
+     */
+    private function criaTemporadas(int $qtdTemporadas, $serie, int $epPorTemporada): void
+    {
+        for ($i = 1; $i <= $qtdTemporadas; $i++) {
+            $temporada = $serie->temporadas()->create( ['numero' => $i] );
+
+            $this->criaEpisodios( $epPorTemporada, $temporada );
+
+        }
+    }
+
+    /**
+     * @param int $epPorTemporada
+     * @param $temporada
+     */
+    private function criaEpisodios(int $epPorTemporada, $temporada): void
+    {
+        for ($j = 1; $j <= $epPorTemporada; $j++) {
+            $temporada->episodios()->create( ['numero' => $j] );
+        }
     }
 }
