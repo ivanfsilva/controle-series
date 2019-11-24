98f5c3b2ed92337a016254ad9d3752fd766c527e
ivanfsilva <ivanfs27@gmail.com>
2019-11-24 14:31:26 -0300

    Editando series

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -66,6 +66,35 @@ Séries
                 nomeSerieEl.hidden = true;
             }
         }
+
+        function editarSerie(serieId) {
+
+           let formData = new FormData();
+           const nome = document
+               .querySelector(`#input-nome-serie-${serieId} > input`)
+               .value;
+
+           const token = document.querySelector(`input[name="_token"]`).value;
+
+           formData.append('nome', nome);
+           formData.append('_token', token);
+
+            // enviar para uma rota
+            const url = `/series/${serieId}/editaNome`;
+
+            // fazer um ama requisição
+
+            fetch(url, {
+                body: formData,
+                method: 'POST'
+            }).then(() =>
+            {
+                toggleInput(serieId);
+                document.getElementById(`nome-serie-${serieId}`).textContent = nome;
+
+            });
+        }
+
     </script>
 
 @endsection
