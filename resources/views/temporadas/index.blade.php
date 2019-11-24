12e5badf95e2190f625b125557a2c9124d25c4c5
ivanfsilva <ivanfs27@gmail.com>
2019-11-24 14:47:47 -0300

    Exibindo o numero de episodios

--- resources/views/temporadas/index.blade.php
+++ resources/views/temporadas/index.blade.php
@@ -7,7 +7,15 @@
 @section('conteudo')
 
     @foreach($temporadas as $temporada)
-        <li class="list-group-item">Temporada {{ $temporada->numero }}</li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a href="#">
+                Temporada {{ $temporada->numero }}
+            </a>
+            <scan class="badge badge-secondary">
+                {{ $temporada->episodios->count() }}
+            </scan>
+        </li>
+
     @endforeach()
 
 @EndSection
