1bb9b698282e2fe1701acd03ebd062b2707e1230
ivanfsilva <ivanfs27@gmail.com>
2019-11-24 15:16:48 -0300

    Listando episodios

--- resources/views/temporadas/index.blade.php
+++ resources/views/temporadas/index.blade.php
@@ -8,11 +8,11 @@
 
     @foreach($temporadas as $temporada)
         <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="#">
+            <a href="/temporadas/{{ $temporada->id }}/episodios">
                 Temporada {{ $temporada->numero }}
             </a>
             <scan class="badge badge-secondary">
-                {{ $temporada->episodios->count() }}
+                0 / {{ $temporada->episodios->count() }}
             </scan>
         </li>
 
